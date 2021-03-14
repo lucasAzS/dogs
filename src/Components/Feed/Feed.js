37dc15ff4a127d70e528892e31accd34e895f128
Lucas Azambuja Santos <lucasazsantos@gmail.com>
2021-03-14 19:54:07 -0300

    Feed Modal 1 & 2

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -4,10 +4,13 @@ import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
   return (
     <div>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
