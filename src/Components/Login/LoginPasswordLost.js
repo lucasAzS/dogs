7b67a9b66118cfd40cd944b393ff3df4634acf0d
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-17 21:04:07 -0300

    perdeu senha 1 & 2

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,7 +1,48 @@
 import React from 'react';
 
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
+import Error from '../Helper/Error';
+
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = useForm();
+  const { data, error, loading, request } = useFetch();
+
+  //console.log(window.location);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'), //TODO: atualizar url quando fizer a hospedagem
+      });
+      await request(url, options);
+    }
+  }
+  return (
+    <section>
+      <h1 className='title'>Perdeu a Senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label='Email / Usuário' type='text' name='email' {...login} />
+          {/* {email.value} */}
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      {error && <Error error={error} />}
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+
+import { PASSWORD_RESET } from '../../api';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import Error from '../Helper/Error';
 
 const LoginPasswordReset = () => {
-  return <div></div>;
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { res } = await request(url, options);
+      if (res.ok) navigate('/login');
+    }
+  }
+
+  return (
+    <div>
+      {/* <p>{key}</p>
+      <p>{login}</p> */}
+      <h1 className='title'>Resete a senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label='Nova Senha'
+          type='password'
+          name='password'
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      {error && <Error error={error} />}
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
